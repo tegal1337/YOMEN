42af4c8b339395565cd49404011257335f559665
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-08-15 05:11:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   keywords: "awokaokwaokwao",
-  comments: "Hello Subscribe my channel",
+//   comments: "Hello Subscribe my channel",
   usernamegoogle: "username",
   passwordgoogle: "pass",
 };
