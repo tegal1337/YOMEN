eb2ddcb4b7afb8ab988200a56d0497ecdcee3cc7
taqin <abdulmuttaqin456@gmail.com>
2022-04-08 04:00:00 +0700

    configure multiple account

---
+++ multiakun.js
@@ -0,0 +1,12 @@
+module.exports = {
+    data: [{
+        username: "account1",
+        password: "pass1"
+    }, {
+        username: "account1",
+        password: "pass1"
+    }, {
+        username: "account1",
+        password: "pass1"
+    }, ]
+};
