26573f62d6f7d6fb3196a02384f9ce62a7d439df
taqin <abdulmuttaqin456@gmail.com>
2022-05-19 12:51:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   keywords: ["Watford vs Arsenal","Roma vs Atalanta","Guinandra Jatikusumo","Cagliari vs Lazio","Aston Villa","Liga Indonesia","Naura","Juventus vs Spezia","Tokushima Vortis vs Verdy","Kris Wu","Lippo Mall Kemang","DJ Una","Leonardo DiCaprio","PSG vs Nice","Bus terbakar di Tol Pandaan","UFC 272","Liga 3","Uus","Fabio Quartararo"],
-  comments: ["hello","let's be a friend","subs to subs ahaha",],
+  comments: ["hello","let's be a friend","subs to subs ahaha"],
   usernamegoogle: "username",
   passwordgoogle: "pass",
   delaycomment: 60, // delay ( per second )
   trending: false,
   copycomment:true, // false if you want use comment above , i mean from array
-  multiakun: false, // true if you want to you multiple account 
+  multiakun: false, // true if you want to use multiple account 
 };
