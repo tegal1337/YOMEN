94c381785aa76b9a75932a7720ef102ef86270f5
Coppo92 <cartavelina92@icloud.com>
2022-07-12 13:35:39 +0200

    e

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  keywords: ["Watford vs Arsenal","Roma vs Atalanta","Guinandra Jatikusumo","Cagliari vs Lazio","Aston Villa","Liga Indonesia","Naura","Juventus vs Spezia","Tokushima Vortis vs Verdy","Kris Wu","Lippo Mall Kemang","DJ Una","Leonardo DiCaprio","PSG vs Nice","Bus terbakar di Tol Pandaan","UFC 272","Liga 3","Uus","Fabio Quartararo"],
-  comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "username",
-  passwordgoogle: "pass",
+  keywords: ["foodporn,cucina,ricette,challenge,bio,10000,vegana,vegan,homemaid,eat,healthy, fit fun porn,ricette fit, dieta"],
+  comments: ["Video Incredibile, consiglio a tutti di vederlo!😊"],
+  usernamegoogle: "mktgfdm@gmail.com",
+  passwordgoogle: "Quasani19!",
   delaycomment: 60, // delay ( per second )
   trending: false,
   copycomment:true, // false if you want use comment above , i mean from array
   multiakun: false, // true if you want to use multiple account ,
-  userdatadir : "fdciabdul"
+  userdatadir : "fattoria della mandorla"
 };
+
