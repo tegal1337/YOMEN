613966d4e2cd9ac7f1f4e37d88166a1268a2a6ba
insideourheart <abdulmuttaqin456@gmail.com>
2022-10-06 22:25:47 +0700

    delete

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   keywords: ["shorts", "pass"],
   comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "username",
-  passwordgoogle: "password",
+  usernamegoogle: "chanmenme2",
+  passwordgoogle: "mana1414",
   delaycomment: 60, // delay ( per second )
-  trending: false,
-  copycomment:false, // false if you want use comment above , i mean from array
+  trending: true,
+  copycomment:true, // false if you want use comment above , i mean from array
   userdatadir : "fdciabdul"
 };
