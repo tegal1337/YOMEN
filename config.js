db2139bc1fe325ab1ab849bc36d01dc93ff9a819
taqin <abdulmuttaqin456@gmail.com>
2022-10-07 04:43:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   keywords: ["shorts", "pass"],
   comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "chanmenme2",
-  passwordgoogle: "mana1414",
+  usernamegoogle: "",
+  passwordgoogle: "",
   delaycomment: 60, // delay ( per second )
   trending: true,
   copycomment:true, // false if you want use comment above , i mean from array
