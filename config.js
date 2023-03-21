c247e689ab173ea50aef5fdd183c46674eb0255d
taqin <abdulmuttaqin456@gmail.com>
2023-03-21 08:02:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   keywords: ["national geographic", "pass"],
   comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "chanmenme2",
-  passwordgoogle: "calang1414",
+  usernamegoogle: "usernamegoogle",
+  passwordgoogle: "your password",
   delaycomment: 60, // delay ( per second )
   trending: true,//false
   copycomment:false, // false if you want use comment above , i mean from array
