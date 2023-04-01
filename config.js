1730e16edcae2e6ab9b343d4fbb73e7217438839
taqin <abdulmuttaqin456@gmail.com>
2023-04-01 21:04:24 +0700

    fix ai comment

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  keywords: ["national geographic", "pass"],
+  comments: ["hello","let's be a friend","subs to subs ahaha"],
+  usernamegoogle: "usernamegoogle",
+  passwordgoogle: "your password",
+  delaycomment: 60, // delay ( per second )
+  trending: true,//false
+  copycomment:false, // false if you want use comment above , i mean from array
+  userdatadir : "fdciabdul",
+  ai : true, // true if you want to use OPEN AI (CHAT-GPT) auto comment
+  apiKey : "",
+};
