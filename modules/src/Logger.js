0b3e07a9e1315485c4c01b5f75af1c0692e25c41
taqin <abdulmuttaqin456@gmail.com>
2024-06-11 09:06:54 +0700

    update Logger.js

--- modules/src/Logger.js
+++ modules/src/Logger.js
@@ -4,6 +4,11 @@ class Logger {
   constructor() {}
 
   log(path, user, link, status, error = null) {
+    const exist = fs.existsSync(path);
+
+    if (!exist) {
+      fs.writeFileSync(path, "");
+    }
     const date = new Date().toLocaleTimeString();
     const err = error ? " | Error: " + error : "";
     const log = `[${date}] | User: ${user} | Link: ${link} | Status: ${status}${err}`;
