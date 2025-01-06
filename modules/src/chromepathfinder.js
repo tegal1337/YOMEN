ec68ff00b48c17cbbd24272bd9def135bd07e830
taqin <abdulmuttaqin456@gmail.com>
2025-01-06 23:01:19 +0700

    update 3 files and delete 19 files

--- modules/src/chromepathfinder.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const karmaChromeLauncher = require('karma-chrome-launcher');
-
-for (const end of ['hrome', 'hromeCanary', 'hromium']) {
-	exports[`c${end}`] = karmaChromeLauncher[`launcher:C${end}`][1].prototype.DEFAULT_CMD[process.platform] || null;
-}
